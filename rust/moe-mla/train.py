296a5e5d41b7ae7710c379a7d49d82790b3a6518
emanuelbertey <52244807+emanuelbertey@users.noreply.github.com>
2026-07-02 15:22:48 -0300

    d

--- rust/moe-mla/train.py
+++ rust/moe-mla/train.py
@@ -308,7 +308,7 @@ def main():
             micro += 1
 
             if micro >= grad_accum:
-                grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+                grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 3.0)
                 opt.step()
                 step += 1
                 micro = 0
