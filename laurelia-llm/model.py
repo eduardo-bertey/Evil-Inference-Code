d5feda5636995226e29f514643acc0728e1e2f96
eduardo-bertey <eduardo-bertey@users.noreply.github.com>
2026-08-01 20:20:29 -0300

    config: batch_size 8

--- laurelia-llm/model.py
+++ laurelia-llm/model.py
@@ -21,7 +21,7 @@ class Config:
     emb_num = 32000
     rotary_pct = 0.25
 
-    batch_size: int = 10
+    batch_size: int = 8
     grad_acc: int = 6
     learning_rate: float = 3e-4
     weight_decay: float = 0.1
